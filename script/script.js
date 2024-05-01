ec782f2572945835f6375cb7a8e9cfe8c98f116e
J0P3dro <jpagalhardo@gmail.com>
2024-05-01 12:33:54 -0300

    Correção

--- script/script.js
+++ script/script.js
@@ -150,7 +150,7 @@ function enviarParaWhatsApp() {
 
     const texto = `Nome: ${nome}\nE-mail: ${email}\nTelefone: ${telefone}\nMensagem: ${mensagem}`;
     const textoCodificado = encodeURIComponent(texto);
-    const numeroWhatsApp = '5581982629228'; // Insira o número de telefone do WhatsApp aqui (apenas números)
+    const numeroWhatsApp = '5585997272238'; // Insira o número de telefone do WhatsApp aqui (apenas números)
     const url = `https://wa.me/${numeroWhatsApp}?text=${textoCodificado}`;
 
     window.open(url, '_blank');
